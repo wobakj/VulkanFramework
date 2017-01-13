fc0de843c6128abbe77ff438e2f47570dc04e55c
Jakob Wagner <wobakjerna@web.de>
2017-01-13 16:09:53 +0100

    add minimal threading application

---
+++ application/include/application_threaded_min.hpp
@@ -0,0 +1,43 @@
+#ifndef APPLICATION_THREADED_MINIMAL_HPP
+#define APPLICATION_THREADED_MINIMAL_HPP
+
+#include <vulkan/vulkan.hpp>
+
+#include "application_threaded.hpp"
+#include "deleter.hpp"
+#include "model.hpp"
+#include "buffer.hpp"
+#include "render_pass.hpp"
+#include "memory.hpp"
+#include "frame_buffer.hpp"
+#include "fence.hpp"
+
+#include <vector>
+#include <atomic>
+
+class ApplicationThreadedMin : public ApplicationThreaded {
+ public:
+  ApplicationThreadedMin(std::string const& resource_path, Device& device, SwapChain const& chain, GLFWwindow*, std::vector<std::string> const& args);
+  ~ApplicationThreadedMin();
+
+ private:
+  void recordDrawBuffer(FrameResource& res) override;
+  void createCommandBuffers(FrameResource& res) override;
+  void updateCommandBuffers(FrameResource& res) override;
+  
+  void createFramebuffers() override;
+  void createFramebufferAttachments() override;
+  void createRenderPasses() override;
+  void createMemoryPools() override;
+  void createPipelines() override;
+  // handle key input
+  // void keyCallback(int key, int scancode, int action, int mods) override;
+  FrameResource createFrameResource() override;
+
+  // path to the resource folders
+  RenderPass m_render_pass;
+  Deleter<VkPipeline> m_pipeline;
+  FrameBuffer m_framebuffer;
+};
+
+#endif
---
+++ application/source/application_threaded_min.cpp
@@ -0,0 +1,184 @@
+#include "application_threaded_min.hpp"
+
+#include "launcher.hpp"
+#include "image.hpp"
+#include "buffer.hpp"
+#include "shader.hpp"
+#include "texture_loader.hpp"
+#include "model_loader.hpp"
+
+// c++ warpper
+#include <vulkan/vulkan.hpp>
+
+#include <glm/gtc/type_precision.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/matrix_inverse.hpp>
+
+#include <iostream>
+#include <chrono>
+
+
+ApplicationThreadedMin::ApplicationThreadedMin(std::string const& resource_path, Device& device, SwapChain const& chain, GLFWwindow* window, std::vector<std::string> const& args) 
+ :ApplicationThreaded{resource_path, device, chain, window, args, chain.numImages() - 1}
+ ,m_pipeline{m_device, vkDestroyPipeline}
+{  
+  m_shaders.emplace("simple", Shader{m_device, {m_resource_path + "shaders/quad_vert.spv", m_resource_path + "shaders/solid_frag.spv"}});
+
+  createFrameResources();
+
+  resize();
+
+  startRenderThread();
+}
+
+ApplicationThreadedMin::~ApplicationThreadedMin() {
+  shutDown();
+}
+
+FrameResource ApplicationThreadedMin::createFrameResource() {
+  FrameResource res{m_device};
+  createCommandBuffers(res);
+  return res;
+}
+
+void ApplicationThreadedMin::createCommandBuffers(FrameResource& res) {
+  res.command_buffers.emplace("gbuffer", m_device.createCommandBuffer("graphics", vk::CommandBufferLevel::eSecondary));
+}
+
+void ApplicationThreadedMin::updateCommandBuffers(FrameResource& res) {
+
+  res.command_buffers.at("gbuffer").reset({});
+
+  vk::CommandBufferInheritanceInfo inheritanceInfo{};
+  inheritanceInfo.renderPass = m_render_pass;
+  inheritanceInfo.framebuffer = m_framebuffer;
+  inheritanceInfo.subpass = 0;
+
+  // first pass
+  res.command_buffers.at("gbuffer").begin({vk::CommandBufferUsageFlagBits::eRenderPassContinue | vk::CommandBufferUsageFlagBits::eSimultaneousUse, &inheritanceInfo});
+
+  res.command_buffers.at("gbuffer").bindPipeline(vk::PipelineBindPoint::eGraphics, m_pipeline.get());
+
+  res.command_buffers.at("gbuffer").draw(3, 1, 0, 0);
+
+  res.command_buffers.at("gbuffer").end();
+}
+
+void ApplicationThreadedMin::recordDrawBuffer(FrameResource& res) {
+
+  res.command_buffers.at("draw").reset({});
+
+  res.command_buffers.at("draw").begin({vk::CommandBufferUsageFlagBits::eSimultaneousUse | vk::CommandBufferUsageFlagBits::eOneTimeSubmit});
+  res.command_buffers.at("draw").beginRenderPass(m_framebuffer.beginInfo(), vk::SubpassContents::eSecondaryCommandBuffers);
+  // execute gbuffer creation buffer
+  res.command_buffers.at("draw").executeCommands({res.command_buffers.at("gbuffer")});
+  
+  res.command_buffers.at("draw").endRenderPass();
+  // make sure rendering to image is done before blitting
+  // barrier is now performed through renderpass dependency
+
+  vk::ImageBlit blit{};
+  blit.srcSubresource = img_to_resource_layer(m_images.at("color_2").info());
+  blit.dstSubresource = img_to_resource_layer(m_swap_chain.imgInfo());
+  blit.srcOffsets[1] = vk::Offset3D{int(m_swap_chain.extent().width), int(m_swap_chain.extent().height), 1};
+  blit.dstOffsets[1] = vk::Offset3D{int(m_swap_chain.extent().width), int(m_swap_chain.extent().height), 1};
+  res.command_buffers.at("draw").blitImage(m_images.at("color_2"), m_images.at("color_2").layout(), m_swap_chain.images().at(res.image), m_swap_chain.layout(), {blit}, vk::Filter::eNearest);
+
+  res.command_buffers.at("draw").end();
+}
+
+void ApplicationThreadedMin::createFramebuffers() {
+  m_framebuffer = FrameBuffer{m_device, {&m_images.at("color_2")}, m_render_pass};
+}
+
+void ApplicationThreadedMin::createRenderPasses() {
+  sub_pass_t pass_1({0},{});
+  m_render_pass = RenderPass{m_device, {m_images.at("color_2").info()}, {pass_1}};
+}
+
+void ApplicationThreadedMin::createPipelines() {
+  
+  vk::PipelineInputAssemblyStateCreateInfo inputAssembly{};
+  inputAssembly.topology = vk::PrimitiveTopology::eTriangleList;
+
+  vk::Viewport viewport{};
+  viewport.width = (float) m_swap_chain.extent().width;
+  viewport.height = (float) m_swap_chain.extent().height;
+  viewport.minDepth = 0.0f;
+  viewport.maxDepth = 1.0f;
+
+  vk::Rect2D scissor{};
+  scissor.extent = m_swap_chain.extent();
+
+  vk::PipelineViewportStateCreateInfo viewportState{};
+  viewportState.viewportCount = 1;
+  viewportState.pViewports = &viewport;
+  viewportState.scissorCount = 1;
+  viewportState.pScissors = &scissor;
+
+  vk::PipelineRasterizationStateCreateInfo rasterizer{};
+  rasterizer.lineWidth = 1.0f;
+  rasterizer.cullMode = vk::CullModeFlagBits::eNone;
+
+  vk::PipelineMultisampleStateCreateInfo multisampling{};
+
+  vk::PipelineColorBlendAttachmentState colorBlendAttachment{};
+  colorBlendAttachment.colorWriteMask = vk::ColorComponentFlagBits::eR | vk::ColorComponentFlagBits::eG | vk::ColorComponentFlagBits::eB | vk::ColorComponentFlagBits::eA;
+  colorBlendAttachment.blendEnable = VK_FALSE;
+  std::vector<vk::PipelineColorBlendAttachmentState> states{colorBlendAttachment};
+
+  vk::PipelineColorBlendStateCreateInfo colorBlending{};
+  colorBlending.attachmentCount = uint32_t(states.size());
+  colorBlending.pAttachments = states.data();
+
+  vk::PipelineDepthStencilStateCreateInfo depthStencil{};
+
+  auto pipelineInfo = m_shaders.at("simple").startPipelineInfo();
+
+  vk::PipelineVertexInputStateCreateInfo vert_info{};
+  pipelineInfo.pVertexInputState = &vert_info;
+  pipelineInfo.pInputAssemblyState = &inputAssembly;
+  pipelineInfo.pViewportState = &viewportState;  
+  pipelineInfo.pRasterizationState = &rasterizer;
+  pipelineInfo.pMultisampleState = &multisampling;
+  pipelineInfo.pDepthStencilState = nullptr; // Optional
+  pipelineInfo.pColorBlendState = &colorBlending;
+  pipelineInfo.pDynamicState = nullptr; // Optional
+  pipelineInfo.pDepthStencilState = &depthStencil;
+  
+  pipelineInfo.renderPass = m_render_pass;
+  pipelineInfo.subpass = 0;
+
+  pipelineInfo.flags = vk::PipelineCreateFlagBits::eAllowDerivatives;
+  if (m_pipeline) {
+    pipelineInfo.flags |= vk::PipelineCreateFlagBits::eDerivative;
+    // insert previously created pipeline here to derive this one from
+    pipelineInfo.basePipelineHandle = m_pipeline.get();
+    pipelineInfo.basePipelineIndex = -1; // Optional
+  }
+  auto pipelines = m_device->createGraphicsPipelines(vk::PipelineCache{}, {pipelineInfo});
+  m_pipeline = pipelines[0];
+}
+
+void ApplicationThreadedMin::createMemoryPools() {
+  m_device->waitIdle();
+  // allocate pool for 5 32x4 fb attachments
+  m_device.reallocateMemoryPool("framebuffer", m_images.at("color_2").memoryTypeBits(), vk::MemoryPropertyFlagBits::eDeviceLocal, m_images.at("color_2").size() * 5);
+  
+  m_images.at("color_2").bindTo(m_device.memoryPool("framebuffer"));
+}
+
+void ApplicationThreadedMin::createFramebufferAttachments() {
+  auto extent = vk::Extent3D{m_swap_chain.extent().width, m_swap_chain.extent().height, 1}; 
+ 
+  m_images["color_2"] = m_device.createImage(extent, m_swap_chain.format(), vk::ImageTiling::eOptimal, vk::ImageUsageFlagBits::eColorAttachment | vk::ImageUsageFlagBits::eTransferSrc);
+  m_images.at("color_2").transitionToLayout(vk::ImageLayout::eTransferSrcOptimal);
+
+  createMemoryPools();
+}
+///////////////////////////// misc functions ////////////////////////////////
+
+// exe entry point
+int main(int argc, char* argv[]) {
+  Launcher::run<ApplicationThreadedMin>(argc, argv);
+}
